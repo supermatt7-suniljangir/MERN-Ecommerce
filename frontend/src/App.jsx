ff288615335e21d083bb3d7a9fea8661e29c7d87
sunil jangid <matt348806@gmail.com>
2024-10-17 22:10:33 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    
+      <div>
+        <p className='text-stone-900 bg-red-500'>hii there</p>
+      </div>
+    
+  )
+}
+
+export default App
